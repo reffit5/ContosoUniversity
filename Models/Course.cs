186cc5953826ca09cdedbcaec3dddeab16c4555d
reffit5 <reffit5@JB202-L21-W-D>
2020-02-27 14:43:31 -0500

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        [Required]
+        [ConcurrencyCheck]
+        [MaxLength(24, ErrorMessage = "The maximum length is 24 characters")]
+        [MinLength(5, ErrorMessage = "The minimum length is 5 characters")]
+        [Index(IsUnique = true)]
+        public string Title { get; set; }
+        [Range(1, 6)]
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        [Display(Name = "Enrollment ID")]
+        public int EnrollmentID { get; set; }
+        [Index]
+        [Display(Name = "Course ID")]
+        public int CourseID { get; set; }
+        [Index]
+        [Display(Name = "Student")]
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
